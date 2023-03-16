efbe0595b5de5f582e43196181cb6549a033e83e
Wildan <wildanwigenta52@gmail.com>
2023-03-16 19:46:40 +0700

    commit

---
+++ database/migrations/2023_01_23_140506_create_lelangs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lelangs', function (Blueprint $table) {
+            $table->id('id_lelang');
+            $table->unsignedBigInteger('id_barang');
+            $table->foreign('id_barang')->references('id_barang')->on('barangs')->onDelete('cascade');
+            $table->date('tgl_lelang');
+            $table->integer('harga_akhir');
+            $table->unsignedBigInteger('id');
+            $table->foreign('id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('status',['dibuka','ditutup']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lelangs');
+    }
+};
