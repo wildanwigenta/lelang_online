701274e45fa133e7fb3dbf3b1e8defc8b9a92650
Wildan <wildanwigenta52@gmail.com>
2025-07-17 23:44:33 +0700

    update harga akhir table

--- database/migrations/2023_01_23_140506_create_lelangs_table.php
+++ database/migrations/2023_01_23_140506_create_lelangs_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('id_barang');
             $table->foreign('id_barang')->references('id_barang')->on('barangs')->onDelete('cascade');
             $table->date('tgl_lelang');
-            $table->integer('harga_akhir');
+            $table->integer('harga_akhir')->nullable();
             $table->unsignedBigInteger('id');
             $table->foreign('id')->references('id')->on('users')->onDelete('cascade'); 
             $table->enum('status',['dibuka','ditutup']);
